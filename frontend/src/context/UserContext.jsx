dce674a004379ee61667a2e421823e1e79472782
jasxyke01 <jaspher.cortez@growthops.asia>
2023-10-03 15:22:09 +0800

    updated the user session management for better efficiency

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import axiosClient from "../utils/axios";
 import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 const UserContext = React.createContext(null);
+const cookies = new Cookies({ path: "/", expires: 0 });
 
 export function useUser() {
   return useContext(UserContext);
@@ -11,12 +13,24 @@ export function useUser() {
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
+
+  const saveUser = (user) => {
+    setUser(user);
+    cookies.set("user", JSON.stringify(user));
+  };
+
   useEffect(() => {
-    console.log("is this running");
+    const userCookies = cookies.get("user");
+    if (userCookies) {
+      console.log("has user saved in cookies");
+      console.log(userCookies);
+      setUser(userCookies);
+      return;
+    }
     axiosClient
       .post("/me")
       .then((res) => {
-        setUser(res.data);
+        saveUser(res.data);
         console.log(res.data);
       })
       .catch((error) => {
